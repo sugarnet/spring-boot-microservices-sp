836314df5f804431d13a671f20852b5d8ec0fd47
dscifo <diego.scifo@globallogic.com>
2023-07-03 09:23:12 -0300

    Add get config endpoint Add dev/prod profiles Connect config server with GitHub repository

--- spring-boot-service-items/src/main/java/com/dss/springboot/service/items/controller/ItemController.java
+++ spring-boot-service-items/src/main/java/com/dss/springboot/service/items/controller/ItemController.java
@@ -8,7 +8,12 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,19 +21,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+@RefreshScope
 @RestController
 @RequestMapping("/api/v1")
 public class ItemController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ItemController.class);
+
+    @Value("${configuration.text}")
+    private String text;
     private final ItemService itemService;
     private final CircuitBreakerFactory circuitBreakerFactory;
 
-    public ItemController(@Qualifier("itemServiceFeign") ItemService itemService, CircuitBreakerFactory circuitBreakerFactory) {
+    private final Environment env;
+
+    public ItemController(@Qualifier("itemServiceFeign") ItemService itemService, CircuitBreakerFactory circuitBreakerFactory, Environment env) {
         this.itemService = itemService;
         this.circuitBreakerFactory = circuitBreakerFactory;
+        this.env = env;
     }
 
     @GetMapping("/list")
@@ -61,6 +75,18 @@ public class ItemController {
         return getAlternativeItem(id, amount, new Throwable("Using alternative method from the other side..."));
     }
 
+    @GetMapping("/config")
+    public ResponseEntity<?> config(@Value("${server.port}") String port) {
+        Map<String, String> json = new HashMap<>();
+        json.put("text", text);
+        json.put("port", port);
+
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+            json.put("author", env.getProperty("configuration.author.name"));
+            json.put("mail", env.getProperty("configuration.author.mail"));
+        }
+        return new ResponseEntity<>(json, HttpStatus.OK);
+    }
     public Item alternativeMethod(@PathVariable Long id, @PathVariable Integer amount, Throwable e) {
         return getAlternativeItem(id, amount, e);
     }
